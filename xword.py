e2d9a67a24e5c2071ddc7cd6511ddaee9e8da69c
Rob Spears <rob.sp3ars@gmail.com>
2020-02-13 22:40:19 -0500

    Write function to get user input and validate it

--- xword.py
+++ xword.py
@@ -1,18 +1,47 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 """Crossword Solver Program"""
-
-__author__ = "???"
+__author__ = "Rob Spears (GitHub: Forty9Unbeaten)"
 
 # YOUR HELPER FUNCTION GOES HERE
 
 
+def get_input(input_message):
+
+    # determine if user input is valid
+    def is_valid(word):
+        if not word:
+            return False
+        for char in word:
+            if not char.isalpha() and char != ' ':
+                return False
+        return True
+
+    invalid_message = ['There was a problem with your input.\n',
+                       'Please make sure you only include known letters \n',
+                       'and spaces to indicate unknown letters: ']
+
+    word = input(input_message)
+    valid_input = is_valid(word)
+    while not valid_input:
+        word = input(''.join(invalid_message))
+        valid_input = is_valid(word)
+    return word
+
+
+def get_matches(word_list, user_word):
+
+
 def main():
     with open('dictionary.txt') as f:
-        words = f.read().split()
+        word_list = f.read().split()
 
-    test_word = raw_input(
-        'Please enter a word to solve.\nUse spaces to signify unknown letters: ').lower()
+    input_message = ['Please enter a word to solve.\n',
+                     'Use spaces to signify unknown letters: ']
+
+    user_word = get_input(''.join(input_message)).lower()
+
+    matches = get_matches(word_list, user_word)
 
     # YOUR ADDITIONAL CODE HERE
     raise NotImplementedError('Please complete this')
